5955e4499e5a4c38b46c66948b2d9c58e4e67728
VesperQuartz <lilbrown3000@gmail.com>
2026-03-24 21:30:14 +0100

    update packages

--- apps/web.start/src/routes/(auth)/login.tsx
+++ apps/web.start/src/routes/(auth)/login.tsx
@@ -1,41 +1,120 @@
+import { useForm } from "@tanstack/react-form";
 import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@workspace/ui/components/button";
 import { Input } from "@workspace/ui/components/input";
-import { toast } from "sonner";
-import { authClient } from "@/lib/auth-client";
+import { FieldError } from "@workspace/ui/form-error";
+import type { ChangeEvent } from "react";
+import { useTanStackFormMask } from "use-mask-input";
+import { z } from "zod";
+
+const formSchema = z.object({
+	email: z.email(),
+	password: z.string().min(6),
+	phone: z.string(),
+});
 
 export const Route = createFileRoute("/(auth)/login")({
 	component: RouteComponent,
+	validateSearch: z.object({
+		redirect: z.string().optional(),
+	}),
 });
 
 function RouteComponent() {
+	const maskField = useTanStackFormMask();
+	const form = useForm({
+		defaultValues: {
+			email: "",
+			password: "",
+			phone: "",
+		},
+		validators: {
+			onSubmit: formSchema,
+		},
+		onSubmit: (value) => {
+			console.log(JSON.stringify(value.value, null, 2));
+		},
+	});
 	return (
-		<div className="flex p-10">
-			<form
-				className="space-y-1"
-				onSubmit={(e) => {
-					e.preventDefault();
-					const formData = new FormData(e.target);
-					const data = Object.fromEntries(formData);
-					authClient.signIn.email(
-						{
-							email: data.email as string,
-							password: data.password as string,
-							callbackURL: "/",
-						},
-						{
-							onError: (e) => {
-								toast.error(`${e.error.message}`);
+		<form
+			onSubmit={(e) => {
+				e.preventDefault();
+				form.handleSubmit();
+			}}
+		>
+			<div className="m-10 flex w-fit flex-col gap-3">
+				<form.Field name="email">
+					{(field) => {
+						return (
+							<div>
+								<Input
+									name={field.name}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="email"
+									type="email"
+									className="border border-accent"
+								/>
+								<FieldError field={field} />
+							</div>
+						);
+					}}
+				</form.Field>
+				<form.Field name="phone">
+					{(field) => {
+						const phoneProps = maskField(
+							["+234 999 999 9999", "999 9999 9999"],
+							{
+								name: field.name,
+								value: field.state.value,
+								onChange: (
+									event: ChangeEvent<HTMLInputElement, HTMLInputElement>,
+								) => field.handleChange(event.target.value),
+								onBlur: field.handleBlur,
 							},
-						},
-					);
-					console.log(data);
-				}}
-			>
-				<Input type="email" name="email" placeholder="email" />
-				<Input type="password" name="password" placeholder="password" />
-				<Button type="submit">Login</Button>
-			</form>
-		</div>
+							{
+								placeholder: "",
+							},
+						);
+						return (
+							<div>
+								<Input
+									{...phoneProps}
+									type="tel"
+									className="border border-accent"
+								/>
+								<FieldError field={field} />
+							</div>
+						);
+					}}
+				</form.Field>
+				<form.Field name="password">
+					{(field) => {
+						return (
+							<div>
+								<Input
+									name={field.name}
+									onChange={(e) => field.handleChange(e.target.value)}
+									placeholder="password"
+									type="password"
+									className="border border-accent"
+								/>
+								<FieldError field={field} />
+							</div>
+						);
+					}}
+				</form.Field>
+				<form.Subscribe
+					selector={(item) => [item.isSubmitting, item.canSubmit]}
+				>
+					{([isSubmitting, canSubmit]) => {
+						return (
+							<Button disabled={!canSubmit} type="submit">
+								{isSubmitting ? "Logging in..." : "Login"}
+							</Button>
+						);
+					}}
+				</form.Subscribe>
+			</div>
+		</form>
 	);
 }
--- apps/web.start/vite.config.ts
+++ apps/web.start/vite.config.ts
@@ -1,20 +1,28 @@
-import { defineConfig } from "vite"
-import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-import viteReact from "@vitejs/plugin-react"
-import viteTsConfigPaths from "vite-tsconfig-paths"
-import tailwindcss from "@tailwindcss/vite"
-import { nitro } from "nitro/vite"
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
-const config = defineConfig({
-  plugins: [
-    nitro(),
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-})
+const config = defineConfig(({ mode }) => {
+	console.log("mode", mode);
+	return {
+		plugins: [
+			nitro(),
+			babel({
+				presets: [reactCompilerPreset()],
+				include: [/\.(ts|tsx|js|jsx)$/],
+			}),
+			viteTsConfigPaths({
+				projects: ["./tsconfig.json"],
+			}),
+			tailwindcss(),
+			tanstackStart(),
+			viteReact(),
+		],
+	};
+});
 
-export default config
+export default config;
